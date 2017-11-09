c2d64cdd9cad1aef8d2b2375e6be097600602387
NexeyaSGara <sebastien.gara@nexeya.com>
2017-11-09 01:46:43 +0100

    Use tango static

--- CmakeTangoWin.cmake
+++ CmakeTangoWin.cmake
@@ -20,7 +20,7 @@ set(TANGO_INCLUDES "$ENV{TANGO_ROOT}/include")
 # link directories
 set(TANGO_LNK_DIR "$ENV{TANGO_ROOT}/bin;$ENV{TANGO_ROOT}/lib")
 # link files
-set(TANGO_LIBS "$ENV{TANGO_ROOT}/lib/tango.lib;$ENV{TANGO_ROOT}/bin/omniORB4_rt.lib;$ENV{TANGO_ROOT}/bin/omniDynamic4_rt.lib;$ENV{TANGO_ROOT}/bin/COS4_rt.lib;$ENV{TANGO_ROOT}/bin/omnithread_rt.lib;$ENV{TANGO_ROOT}/bin/msvcstub.lib;$ENV{TANGO_ROOT}/lib/pthreadVC2.lib")                                            
+set(TANGO_LIBS "$ENV{TANGO_ROOT}/lib/libtango.lib;$ENV{TANGO_ROOT}/bin/omniORB4_rt.lib;$ENV{TANGO_ROOT}/bin/omniDynamic4_rt.lib;$ENV{TANGO_ROOT}/bin/COS4_rt.lib;$ENV{TANGO_ROOT}/bin/omnithread_rt.lib;$ENV{TANGO_ROOT}/bin/msvcstub.lib;$ENV{TANGO_ROOT}/lib/pthreadVC2.lib")                                            
 set(WIN_LIBS ws2_32 mswsock advapi32 comctl32 odbc32)
 if(MSVC90)
     set(ZMQ_LIB $ENV{TANGO_ROOT}/lib/libzmq-v90-mt-s-4_0_5.lib)
